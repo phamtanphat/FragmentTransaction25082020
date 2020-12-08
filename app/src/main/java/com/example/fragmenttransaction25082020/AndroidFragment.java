a523c06a0d9767dc2eda3e517359dd84a5e6cadc
phatdroid94 <phatdroid94@gmail.com>
2020-12-08 20:23:10 +0700

    pop back stack

--- app/src/main/java/com/example/fragmenttransaction25082020/AndroidFragment.java
+++ app/src/main/java/com/example/fragmenttransaction25082020/AndroidFragment.java
@@ -1,5 +1,6 @@
 package com.example.fragmenttransaction25082020;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,13 +8,23 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+import java.util.Random;
 
 public class AndroidFragment extends Fragment {
 
+    RelativeLayout mBackground;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_android, container, false);
+        View view = inflater.inflate(R.layout.fragment_android, container, false);
+        mBackground = view.findViewById(R.id.relativeBackgroundAndroid);
+        mBackground.setBackgroundColor(Color.rgb(randomColor(),randomColor(),randomColor()));
+        return view;
+    }
+    private int randomColor(){
+        return new Random().nextInt(255) + 1;
     }
 }
--- app/src/main/java/com/example/fragmenttransaction25082020/MainActivity.java
+++ app/src/main/java/com/example/fragmenttransaction25082020/MainActivity.java
@@ -13,7 +13,7 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     FragmentManager mFragmentManager;
-    Button mBtnAddAndroid, mBtnAddIos, mBtnReplaceAndroid, mBtnReplaceIos;
+    Button mBtnAddAndroid, mBtnAddIos, mBtnReplaceAndroid, mBtnReplaceIos , mBtnRemoveAndroid, mBtnRemoveIos , mBtnPopBackStack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +25,17 @@ public class MainActivity extends AppCompatActivity {
         mBtnAddAndroid = findViewById(R.id.buttonAddAndroid);
         mBtnReplaceAndroid = findViewById(R.id.buttonReplaceAndroid);
         mBtnReplaceIos = findViewById(R.id.buttonReplaceios);
+        mBtnRemoveIos = findViewById(R.id.buttonRemoveios);
+        mBtnRemoveAndroid = findViewById(R.id.buttonRemoveAndroid);
+        mBtnPopBackStack = findViewById(R.id.buttonPopBackStack);
 
         mBtnAddIos.setOnClickListener(onClickListener);
         mBtnAddAndroid.setOnClickListener(onClickListener);
         mBtnReplaceAndroid.setOnClickListener(onClickListener);
         mBtnReplaceIos.setOnClickListener(onClickListener);
+        mBtnRemoveIos.setOnClickListener(onClickListener);
+        mBtnRemoveAndroid.setOnClickListener(onClickListener);
+        mBtnPopBackStack.setOnClickListener(onClickListener);
     }
 
     private OnClickListener onClickListener = new OnClickListener() {
@@ -40,11 +46,13 @@ public class MainActivity extends AppCompatActivity {
             switch (v.getId()) {
                 case R.id.buttonAddAndroid:
                     fragment = new AndroidFragment();
-                    fragmentTransaction.add(R.id.liearlayoutContainer, fragment);
+                    fragmentTransaction.add(R.id.liearlayoutContainer, fragment , "fragmentAndroid");
+                    fragmentTransaction.addToBackStack("FragmentAndroid");
                     break;
                 case R.id.buttonAddios:
                     fragment = new IosFragment();
-                    fragmentTransaction.add(R.id.liearlayoutContainer, fragment);
+                    fragmentTransaction.add(R.id.liearlayoutContainer, fragment , "fragmentIos");
+                    fragmentTransaction.addToBackStack("FragmentIos");
                     break;
                 case R.id.buttonReplaceAndroid:
                     fragment = new AndroidFragment();
@@ -53,6 +61,13 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.buttonReplaceios :
                     fragment = new IosFragment();
                     fragmentTransaction.replace(R.id.liearlayoutContainer, fragment);
+                    break;
+                case R.id.buttonRemoveAndroid:
+                    fragment = (AndroidFragment) mFragmentManager.findFragmentByTag("fragmentAndroid");
+                    if (fragment != null) fragmentTransaction.remove(fragment);
+                    break;
+                case R.id.buttonPopBackStack:
+                    mFragmentManager.popBackStack(1,FragmentManager.POP_BACK_STACK_INCLUSIVE);
                     break;
             }
             fragmentTransaction.commit();
